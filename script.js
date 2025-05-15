acd60cc72d04cf5b1f22051f0147b1abcf3da7a4
herreriajhonyo <herreriajhonyo@gmail.com>
2025-05-15 16:08:30 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// animaciones.js
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+document.querySelectorAll('section').forEach(section => {
+  observer.observe(section);
+});
+
+document.querySelectorAll('section, .proximamente').forEach(el => {
+  observer.observe(el);
+});
